d62bc2c62c46ec3c56c6ab8a7402dd2cc52c70cf
zjl <zhangjinlong@common.com>
2020-05-15 13:28:14 +0800

    首次体积哦啊

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * author: king long
+ * date: 2020/5/15 13:20
+ * desc: use for java11
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
